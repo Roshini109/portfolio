b4c6b4f0aa79fd2224a7715f811a91ce3cf239f7
Roshini Dias <jbrnd21@gmail.com>
2023-09-02 23:41:35 +0530

    dark mode and light mode developed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import About from "./components/intro/about/About";
 import Productlist from "./components/productList/Productlist";
+import Toggle from "./components/toggle/Toggle";
+import { ThemeContext } from "./context";
+import { useContext } from "react";
 
 
 function App() {
+
+  const theme = useContext(ThemeContext)
+  const darkMode =theme.state.darkMode;
+
   return (
-    <div>
+    <div style={{backgroundColor: darkMode ? "#222" : "white", color: darkMode && "white"}}>
+      <Toggle/>
       <Intro/>
       <About/>
       <Productlist/>
---
+++ src/components/toggle/Toggle.jsx
@@ -0,0 +1,19 @@
+import './toggle.css'
+import { useContext } from 'react'
+import {ThemeContext} from "../../context"
+
+export default function Toggle() {
+
+const theme = useContext(ThemeContext)
+
+const handleClick =() =>{
+  theme.dispatch({type:"TOGGLE"})
+}
+
+  return (
+    <div className="t">
+        <img src="/assets/lightDark.png" alt="" className="tIcon" />
+        <div className="tBtn" onClick={handleClick} style={{left:theme.state.darkMode ? 0:25}}></div>
+    </div>
+  )
+}
