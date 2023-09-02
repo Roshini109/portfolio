a28f86d3aba77484f150ae82f3c67bac41dce76c
Roshini Dias <jbrnd21@gmail.com>
2023-09-02 12:37:40 +0530

    Create About_Me  page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Intro from "./components/intro/Intro";
+import About from "./components/intro/about/About";
 
 
 function App() {
   return (
     <div>
       <Intro/>
+      <About/>
     </div>
   );
 }
